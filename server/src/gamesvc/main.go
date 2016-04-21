e2233e808b7b1de69f24f18d3638e0331c0253e0
xubo <sunicdavy@qq.com>
2016-04-21 09:15:19 +0800

    游戏服务器的网关日志简化

--- server/src/gamesvc/main.go
+++ server/src/gamesvc/main.go
@@ -15,14 +15,36 @@ import (
 
 var log *golog.Logger = golog.New("main")
 
+var msgLogFilter = make(map[uint32]*cellnet.MessageMeta)
+
+func addMsgLogBlock(msgName string) {
+	meta := cellnet.MessageMetaByName(msgName)
+
+	if meta == nil {
+		log.Errorf("msg log block not found: %s", msgName)
+		return
+	}
+
+	msgLogFilter[meta.ID] = meta
+
+}
+
 func main() {
 
 	table.LoadServiceTable()
 
+	addMsgLogBlock("coredef.UpstreamACK")
+	addMsgLogBlock("coredef.DownstreamACK")
+
 	// 屏蔽socket层消息日志, 避免重复
 	socket.SetMessageLogHook(func(info *socket.MessageLogInfo) bool {
 
-		return false
+		// 找到
+		if _, ok := msgLogFilter[info.ID]; ok {
+			return false
+		}
+
+		return true
 
 	})
 
