67a2bdfddb88b26e9adadf9c419e21dc1712e824
davyxu <sunicdavy@sina.com>
2016-05-08 22:31:15 +0800

    配置

--- tool/update3rd.bat
+++ tool/update3rd.bat
@@ -1,16 +1,13 @@
 : ×¼±¸MyUpdate3rd.bat, ÌîÈëcall update3rd.bat path\to\githubcode
 set GOPATH=%1%
-go install github.com/golang/protobuf/protoc-gen-go
+go get -u -v github.com/golang/protobuf/protoc-gen-go
+go get -u -v github.com/davyxu/pbmeta/protoc-gen-meta
+go get -u -v github.com/davyxu/cellnet/protoc-gen-msg
+go get -u -v github.com/davyxu/protoc-gen-sharpnet
+go get -u -v github.com/davyxu/tabtoy
+
 copy %GOPATH%\bin\protoc-gen-go.exe .\*.*
-
-go install github.com/davyxu/pbmeta/protoc-gen-meta
 copy %GOPATH%\bin\protoc-gen-meta.exe .\*.*
-
-go install github.com/davyxu/cellnet/protoc-gen-msg
 copy %GOPATH%\bin\protoc-gen-msg.exe .\*.*
-
-go install github.com/davyxu/protoc-gen-sharpnet
 copy %GOPATH%\bin\protoc-gen-sharpnet.exe .\*.*
-
-go install github.com/davyxu/tabtoy
 copy %GOPATH%\bin\tabtoy.exe .\*.*
